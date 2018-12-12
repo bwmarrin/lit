e43652a300bb2ee0e0dbe1b2b345674cb5b7d594
Bruce Marriner <bruce@sqls.net>
2018-12-12 10:06:35 -0600

    Keeping it simple

--- logger.go
+++ logger.go
@@ -14,22 +14,20 @@ const (
 
 	// For unrecoverable errors where you would be unable to continue to
 	// current scope of code.
-	Error int = iota
+	ErrorLevel int = iota
 
 	// For non-critical errors that do not require you to abort/exit from the
 	// current scope of code.
-	Warning
+	WarningLevel
 
 	// For non-error "informational" logging.
-	Informational
+	InformationalLevel
 
 	// For any type of verbose debug specific logging.
-	Debug
+	DebugLevel
 )
 
-// Logger is the main struct of the lit package.  It holds all logging config
-// options and methods.
-type Logger struct {
+var (
 	// Prefix is a string that is added to the start of any logged messages.
 	Prefix string
 
@@ -38,37 +36,35 @@ type Logger struct {
 
 	// out is the output io.Writer where messages are wrote to.
 	out io.Writer
-}
+)
 
 // New returns a new *Logger struct with default options.
 // You can (eventually) set options using the various currently non-existant
 // option functions :)
-func New(options ...func(*Logger)) *Logger {
-	l := Logger{}
-	l.Prefix = `LIT`
-	l.LogLevel = 0
-	l.out = os.Stderr
-	return &l
+func init() {
+	Prefix = `LIT`
+	LogLevel = 0
+	out = os.Stderr
 }
 
 // Error logs a Error level message
-func (l Logger) Error(format string, a ...interface{}) {
-	l.Log(Error, 2, format, a...)
+func Error(format string, a ...interface{}) {
+	Log(ErrorLevel, 2, format, a...)
 }
 
 // Warn logs a Warning level message
-func (l Logger) Warn(format string, a ...interface{}) {
-	l.Log(Warning, 2, format, a...)
+func Warn(format string, a ...interface{}) {
+	Log(WarningLevel, 2, format, a...)
 }
 
 // Info logs a Informational level message
-func (l Logger) Info(format string, a ...interface{}) {
-	l.Log(Informational, 2, format, a...)
+func Info(format string, a ...interface{}) {
+	Log(InformationalLevel, 2, format, a...)
 }
 
 // Debug logs a Debug level message
-func (l Logger) Debug(format string, a ...interface{}) {
-	l.Log(Debug, 2, format, a...)
+func Debug(format string, a ...interface{}) {
+	Log(DebugLevel, 2, format, a...)
 }
 
 // Log formats and writes the provided message to the defined io.Writer output
@@ -77,9 +73,9 @@ func (l Logger) Debug(format string, a ...interface{}) {
 //   calldepth : Distance from the caller
 //   format    : Printf style message format
 //   a ...     : comma separated list of values to pass (like Printf)
-func (l Logger) Log(level int, calldepth int, format string, a ...interface{}) {
+func Log(level int, calldepth int, format string, a ...interface{}) {
 
-	if level > l.LogLevel {
+	if level > LogLevel {
 		return
 	}
 
@@ -94,5 +90,5 @@ func (l Logger) Log(level int, calldepth int, format string, a ...interface{}) {
 
 	msg := fmt.Sprintf(format, a...)
 
-	fmt.Fprintf(l.out, "[%s%d] %s:%d:%s() %s\n", l.Prefix, level, file, line, name, msg)
+	fmt.Fprintf(out, "[%s%d] %s:%d:%s() %s\n", Prefix, level, file, line, name, msg)
 }
